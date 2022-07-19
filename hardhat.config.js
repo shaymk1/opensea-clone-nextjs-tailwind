9ca3565601d89e78066eb5de64ce81e06072208e
Mamosebetsi Kekae <mkekae@gmail.com>
2022-07-19 12:45:51 -0700

    configured hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-waffle");
+
+const fs =require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "536442d6e6114afebcc5cf916ee10607";
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+
+		mumbai: {
+			url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+			accounts: [privateKey],
+		},
+
+		mainnet: {
+			url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+			accounts: [privateKey],
+		},
+	},
+	solidity: "0.8.9",
 };
--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+       
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+       
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -32,23 +27,7 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        
         </div>
       </main>
 
