4fedb82fecfc72ee804ddc8d4bee73576caca79b
Mamosebetsi Kekae <mkekae@gmail.com>
2022-07-20 12:00:41 -0700

    ran tests with hardhat and they passed, and created artifacts and cache folders

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,13 @@
-
 //require("@nomiclabs/hardhat-waffle");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomiclabs/hardhat-ethers");
 
-const fs =require("fs");
-const PRIVATE_KEY = fs.readFileSync(".secret").toString();
-const projectId = "536442d6e6114afebcc5cf916ee10607";
+require("dotenv").config();
+
+//const fs =require("fs");
+//const PRIVATE_KEY = fs.readFileSync(".secret").toString();
+//const projectId = "536442d6e6114afebcc5cf916ee10607";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	networks: {
@@ -14,14 +16,15 @@ module.exports = {
 		},
 
 		mumbai: {
-			url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-			accounts: [`${PRIVATE_KEY}`],
+			//url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+			url: process.env.MUMBAI_RPC_URL,
+			accounts: [process.env.PRIVATE_KEY],
 		},
 
-		// mainnet: {
-		// 	url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
-		// 	accounts: [privateKey],
-		// },
+		mainnet: {
+			url: process.env.MAINNET_RPC_URL,
+			accounts: [process.env.PRIVATE_KEY],
+		},
 	},
 	solidity: "0.8.9",
 };
