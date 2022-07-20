fb09beb61ff81c421642969367612bd333f5ad10
Mamosebetsi Kekae <mkekae@gmail.com>
2022-07-20 11:45:26 -0700

    testing if .env is being pushed to github

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,10 @@
-//require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-waffle");
+
+//require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
 
 const fs =require("fs");
-const privateKey = fs.readFileSync(".secret").toString();
+const PRIVATE_KEY = fs.readFileSync(".secret").toString();
 const projectId = "536442d6e6114afebcc5cf916ee10607";
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,13 +15,13 @@ module.exports = {
 
 		mumbai: {
 			url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-			accounts: [privateKey],
+			accounts: [`${PRIVATE_KEY}`],
 		},
 
-		mainnet: {
-			url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
-			accounts: [privateKey],
-		},
+		// mainnet: {
+		// 	url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+		// 	accounts: [privateKey],
+		// },
 	},
 	solidity: "0.8.9",
 };
